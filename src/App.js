da64617489b6ef6fa2de5369f01798596b966c6b
x232859 <suraj.a.patil@external.merckgroup.com>
2023-10-14 12:57:43 +0530

    Implemeted Bill Splitter functionality and added test cases

--- src/App.js
+++ src/App.js
@@ -1,13 +1,81 @@
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [billAmount, setBillAmount] = useState('');
+  const [numberOfPeople, setNumberOfPeople] = useState('');
+  const [tipPercentage, setTipPercentage] = useState('');
+  const [output, setOutput] = useState(null);
+
+  const calculateTotalAmount = () => {
+    const billAmountFloat = parseFloat(billAmount);
+    const numberOfPeopleInt = parseInt(numberOfPeople);
+    const tipAmount = (billAmountFloat * (parseFloat(tipPercentage) / 100)) || 0;
+    const totalBillAmount = billAmountFloat + tipAmount;
+    const eachPersonBill = totalBillAmount / numberOfPeopleInt;
+
+    return { tipAmount, eachPersonBill, totalBillAmount };
+  };
+
+  const handleCalculate = () => {
+    if (billAmount < 0 || numberOfPeople < 0 || tipPercentage < 0) {
+      alert('Please enter non-negative values.');
+      return;
+    }
+
+    const { tipAmount, eachPersonBill, totalBillAmount } = calculateTotalAmount();
+    setOutput({
+      tipAmount: tipAmount.toFixed(2),
+      eachPersonBill: eachPersonBill.toFixed(2),
+      totalBillAmount: totalBillAmount.toFixed(2)
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Bill Splitter</p>
-      </header>
+      <h1>Bill Splitter</h1>
+      <div className="input-group">
+        <label htmlFor="billAmount">Bill Amount: $</label>
+        <input
+          type="number"
+          id="billAmount"
+          value={billAmount}
+          onChange={(e) => setBillAmount(e.target.value)}
+          min="0"
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="numberOfPeople">Number of People: </label>
+        <input
+          type="number"
+          id="numberOfPeople"
+          value={numberOfPeople}
+          onChange={(e) => setNumberOfPeople(e.target.value)}
+          min="1"
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="tipPercentage">Tip Percentage: %</label>
+        <input
+          type="number"
+          id="tipPercentage"
+          value={tipPercentage}
+          onChange={(e) => setTipPercentage(e.target.value)}
+          min="0"
+        />
+      </div>
+      <button onClick={handleCalculate}>Calculate</button>
+
+      {output && (
+        <div className="output">
+          <h2>Output</h2>
+          <p>Tip Amount: ${output.tipAmount}</p>
+          <p>Each Person Bill: ${output.eachPersonBill}</p>
+          <p>Total Amount: ${output.totalBillAmount}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
+/* eslint-disable testing-library/no-node-access */
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('calculates amounts correctly', () => {
+    render(<App />);
+    const billAmountInput = screen.getByLabelText('Bill Amount: $');
+    const numberOfPeopleInput = screen.getByLabelText('Number of People:');
+    const tipPercentageInput = screen.getByLabelText('Tip Percentage: %');
+
+    fireEvent.change(billAmountInput, { target: { value: '100' } });
+    fireEvent.change(numberOfPeopleInput, { target: { value: '5' } });
+    fireEvent.change(tipPercentageInput, { target: { value: '10' } });
+
+    const calculateButton = screen.getByText('Calculate');
+    fireEvent.click(calculateButton);
+
+    const tipAmountText = screen.getByText(/Tip Amount:\s*\$/);
+    const eachPersonBillText = screen.getByText(/Each Person Bill:\s*\$/);
+    const totalAmountText = screen.getByText(/Total Amount:\s*\$/);
+
+    const tipAmount = parseFloat(tipAmountText?.textContent?.split('$')[1]);
+    const eachPersonBill = parseFloat(eachPersonBillText?.textContent?.split('$')[1]);
+    const totalAmount = parseFloat(totalAmountText?.textContent?.split('$')[1]);
+
+    expect(tipAmount).toBeCloseTo(10);
+    expect(eachPersonBill).toBeCloseTo(22);
+    expect(totalAmount).toBeCloseTo(110);
+  });
 });
